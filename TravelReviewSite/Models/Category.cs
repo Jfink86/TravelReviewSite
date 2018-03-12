a86e764362f9da509b578281fe8c4f9141f89eb7
jfink86 <jfink86@yahoo.com>
2018-03-12 13:48:56 -0400

    first commit

---
+++ TravelReviewSite/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewSite.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        //Navigation  property
+
+        public virtual ICollection<Review> Reveiws { get; set; }
+
+    }
+
+}
---
+++ TravelReviewSite/Models/review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; } /*<---Key should always be an int---->*/
+        public string Title { get; set; }
+        public string Description { get; set; }
+       
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; } /*<---navagation property--->*/
+
+    }
+} 
